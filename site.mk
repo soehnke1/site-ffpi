cf27f473ec40474101b34fb75b3e55760dfefaf9
Kevin Quiatkowski <kevin@quiatkowski.net>
2015-11-22 16:47:30 +0100

    Grundlegender USB Support für ausgewählte Geräte

--- site.mk
+++ site.mk
@@ -25,6 +25,46 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
+# USB for some devices
+#ar71xx-generic
+GLUON_TLWR1043_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_TLWR842_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_TLWDR4300_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_TLWR2543_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_WRT160NL_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_DIR825B1_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_TLWR710_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_GLINET_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_WNDR3700_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_WZRHPG450H_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_WZRHPAG300H_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_ARCHERC7_SITE_PACKAGES := mod-usb-core kmod-usb2
+
+#mpc85xx-generic
+GLUON_TLWDR4900_SITE_PACKAGES := mod-usb-core kmod-usb2
+
+# support the usb stack on x86 devices
+# and add a few common USB NICs
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += \
+    kmod-usb-core \
+    kmod-usb2 \
+    kmod-usb-hid \
+    kmod-usb-net \
+    kmod-usb-net-asix \
+    kmod-usb-net-dm9601-ether
+endif
+ifeq ($(GLUON_TARGET),x86-kvm_guest)
+GLUON_SITE_PACKAGES += \
+    kmod-usb-core \
+    kmod-usb2 \
+    kmod-usb-hid \
+    kmod-usb-net \
+    kmod-usb-net-asix \
+    kmod-usb-net-dm9601-ether
+endif
+
+
 DEFAULT_GLUON_RELEASE := 0.7.0
 
 # Allow overriding the release number from the command line
